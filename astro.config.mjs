e8b1978439902e82427a2108f8e00babc88c99d6
mmj <1592446422@qq.com>
2024-08-05 18:51:28 +0800

    动态路由

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    //配置网站域名和路径
     site: 'https://jack123690.github.io',
     base: 'Astro-demo',
+    //配置服务器渲染模式 按需渲染
+    output: 'hybrid',
+
 });
