91a20d13e09e7e9155e194c6057ac98d497dc071
mmj <1592446422@qq.com>
2024-08-05 11:51:29 +0800

    github原始域名访问

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sosmmj.cn',
-    // site: 'https://jack123690.github.io',
-    // base: 'Astro-demo',
+    site: 'https://jack123690.github.io',
+    base: 'Astro-demo',
 });
