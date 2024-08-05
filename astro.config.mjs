61d0b0cbfb1b41885c16113a2c6873e7a7833555
mmj <1592446422@qq.com>
2024-08-05 10:29:37 +0800

    部署到github-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://jack123690.github.io',
+    base: 'Astro-demo',
+});
